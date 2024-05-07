d39a9d0a1d2681086ddb7f1df13a151149c72c77
rahulrsr10 <114585917+rahulrsr10@users.noreply.github.com>
2024-05-07 16:09:28 +0530

    base test class created

--- src/test/java/rahulrode/BaseTest.java
+++ src/test/java/rahulrode/BaseTest.java
@@ -8,20 +8,16 @@ import org.testng.annotations.BeforeMethod;
 
 import io.appium.java_client.android.AndroidDriver;
 
-public class BaseClass {
-	public AndroidDriver driver;
-	BaseClass(AndroidDriver driver){
-		this.driver=driver;
-	}
-	
+public class BaseTest {
+
+	public static AndroidDriver driver;
 	@BeforeMethod
-	public void startup(AndroidDriver driver) throws MalformedURLException {
+	public void startup() throws MalformedURLException {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability("platformName", "Android");
 		capabilities.setCapability("platformVersion", "13");
 		capabilities.setCapability("deviceName", "RahulPhone");
 		capabilities.setCapability("automationName", "UiAutomator2");
-		capabilities.setCapability("autoAcceptAlerts", true);
 		capabilities.setCapability("app", "D:\\APKFiles\\resources\\ApiDemos-debug.apk");
 		driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
 	}
--- src/test/java/rahulrode/TestClass.java
+++ src/test/java/rahulrode/TestClass.java
@@ -1,26 +1,14 @@
 package rahulrode;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
-import io.appium.java_client.android.AndroidDriver;
 
-public class MobilePractice {
-	public static AndroidDriver driver;
-	Assert assertion;
-
-	public static void main(String[] args) throws MalformedURLException, InterruptedException {
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability("platformName", "Android");
-		capabilities.setCapability("platformVersion", "13");
-		capabilities.setCapability("deviceName", "RahulPhone");
-		capabilities.setCapability("automationName", "UiAutomator2");
-		capabilities.setCapability("app", "D:\\APKFiles\\resources\\ApiDemos-debug.apk");
-		driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+public class TestClass extends BaseTest {
+	
+	@Test
+	public void demo() throws InterruptedException {
 		driver.findElement(AppiumBy.accessibilityId("Views")).click();
 		driver.findElement(AppiumBy.accessibilityId("Chronometer")).click();
 		String timer = driver.findElement(AppiumBy.id("io.appium.android.apis:id/chronometer")).getText();
@@ -30,15 +18,12 @@ public class MobilePractice {
 		driver.findElement(AppiumBy.accessibilityId("Stop")).click();
 		timer = driver.findElement(AppiumBy.id("io.appium.android.apis:id/chronometer")).getText();
 		System.out.println(timer);
-		
 		driver.findElement(AppiumBy.accessibilityId("Start")).click();
 		Thread.sleep(3000);
 		driver.findElement(AppiumBy.accessibilityId("Stop")).click();
 		timer = driver.findElement(AppiumBy.id("io.appium.android.apis:id/chronometer")).getText();
 		System.out.println(timer);
-		
-	
-		
 	}
+
 
 }
